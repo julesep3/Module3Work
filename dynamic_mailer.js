c55af6fc9aed9b00b7b254128468ffe10e2db4d9
julesep3 <50899932+julesep3@users.noreply.github.com>
2019-08-23 20:43:50 -0500

    mailer problem almost complete

---
+++ dynamic_mailer.js
@@ -0,0 +1,68 @@
+function printLetter(to) {
+	// from and to object
+	// {name, street, city, state, zip}
+
+	// ========================================
+	// | Zach Mays                            |
+	// | 124 NW 20th                          |
+	// | OKC, OK 73106                        |
+	// |                                      |
+	// |						Name                      |
+	// |						Address                   |
+	// |						City, State, Zip          |
+	// ========================================
+
+	console.log("========================================")
+	console.log("| ", from.name, "                          |")
+	console.log("| ", from.street, "                       |")
+	console.log("| ", `${from.city}, ${from.state}, ${from.zip}`, "                     |")
+	console.log("|                                      |")
+	makeLineWithWidth(letter_width, to.name, left_space)
+	makeLineWithWidth(letter_width, to.street, left_space)
+	makeLineWithWidth(letter_width, csz, left_space)
+	console.log("========================================")
+}
+
+let csz = function() {
+    let x = `${to.city}, ${to.state}, ${to.zip}`
+    return x
+}
+
+//const width = 27;
+
+const letter_width = 40
+const left_space = 10
+function makeLineWithWidth(width, item, desiredLeftSpace) {
+	let line = ""
+	const itemLength = item.length
+	const paddingRight = width - itemLength - desiredLeftSpace
+	line += "|"
+	for(let i = 0; i < desiredLeftSpace; i++) {
+		line += " "
+	}
+    line += item      
+    let remainder = width - (line.length + 1)
+    for(let i = 0; i < remainder; i++) {
+        line += " "
+    }
+    line += "|"
+    console.log(line)	
+}
+
+const from = {
+	name: "Zach Mays",
+	street: "1234 NW 20th",
+	state: "OK",
+	city: "OKC",
+	zip: "73106"
+}
+
+const to = {
+	name: "Bobby Rockers",
+	street: "1589 NW 4th St",
+	state: "OK",
+	city: "OKC",
+	zip: "73105"
+}
+
+console.log(printLetter(to))
